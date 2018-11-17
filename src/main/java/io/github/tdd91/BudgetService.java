639be572bda14936ac0e718a685d4395ce88402a
Bo-Xuan Fan <coffee0127@gmail.com>
2018-11-17 20:50:36 +0800

    extract method: getMonthDays

--- src/main/java/io/github/tdd91/BudgetService.java
+++ src/main/java/io/github/tdd91/BudgetService.java
@@ -103,10 +103,14 @@ public class BudgetService {
     private Map<String, Integer> findBudgetPerDayMap(List<Budget> budgets) {
         return budgets.stream()
             .peek(budget -> {
-                int monthDays = YearMonth.parse(budget.getYearMonth(), DateTimeFormatter.ofPattern("yyyyMM")).lengthOfMonth();
+                int monthDays = getMonthDays(budget);
                 budget.setAmount(budget.getAmount() / monthDays);
             })
             .collect(toMap(Budget::getYearMonth, Budget::getAmount));
+    }
+
+    private int getMonthDays(Budget budget) {
+        return YearMonth.parse(budget.getYearMonth(), DateTimeFormatter.ofPattern("yyyyMM")).lengthOfMonth();
     }
 
 }
